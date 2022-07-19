8a900e96dd4ad2ea9f699534c60ff197bc54b546
mohamedbougarn <mohamedbougarn@gmail.com>
2022-07-19 14:01:12 +0100

    readme update

--- run.py
+++ run.py
@@ -3,10 +3,11 @@ from Detect import Detectcls
 
 #there is meny models in "https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/tf2_detection_zoo.md"
 # or "https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/tf1_detection_zoo.md"
-modelURL="http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz"
+#modelURL="http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz"
+modelURL="http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz"
 
-classFile = 'coco.names'
-imagePath = "data/1.jpg"
+classFile = 'coco.names.fr'
+imagePath = "data/0.jpg"
 #videoPath = "data/video.mp4" # si en test real time avec webcam juste on remplace par 0
 videoPath = 0 # si en test real time avec webcam juste on remplace par 0
 threshould=0.5
@@ -15,8 +16,8 @@ Detect = Detectcls()
 Detect.readClasses(classFile)
 Detect.downloadModel(modelURL)
 Detect.loadModel()
-#Detect.predictImage(imagePath,threshould)
+Detect.predictImage(imagePath,threshould)
 
-Detect.predictVideo(videoPath,threshould)
+#Detect.predictVideo(videoPath,threshould)
 
 detector.createBoundingBox(videoPath)
